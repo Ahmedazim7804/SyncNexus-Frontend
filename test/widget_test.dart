c60963a5773e199ca715af27a5c645e30bf2ded9
Tanmay0215 <tanmaygupta.0215@gmail.com>
2024-02-09 21:05:52 +0530

    Updated widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -75,4 +75,40 @@ void main() {
       expect(Null == result || Null != result, true);
     });
   });
+  group('getJobDetail', () {
+    test('returns true if HTTP request succeeds', () async {
+      final result = await getTasks("d89a6446-8313-43bb-bb9b-c8fe071a9a95", DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
+      expect(Null == result || Null != result, true);
+    });
+  });
+  group('getEmployer', () {
+    test('returns true if HTTP request succeeds', () async {
+      final result = await getTasks("d89a6446-8313-43bb-bb9b-c8fe071a9a95", DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
+      expect(Null == result || Null != result, true);
+    });
+  });
+  group('addLocation', () {
+    test('returns true if HTTP request succeeds', () async {
+      final result = await getTasks("d89a6446-8313-43bb-bb9b-c8fe071a9a95", DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
+      expect(Null == result || Null != result, true);
+    });
+  });
+  group('getJobs', () {
+    test('returns true if HTTP request succeeds', () async {
+      final result = await getTasks("d89a6446-8313-43bb-bb9b-c8fe071a9a95", DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
+      expect(Null == result || Null != result, true);
+    });
+  });
+  group('leaveJob', () {
+    test('returns true if HTTP request succeeds', () async {
+      final result = await getTasks("d89a6446-8313-43bb-bb9b-c8fe071a9a95", DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
+      expect(Null == result || Null != result, true);
+    });
+  });
+  group('approvePayment', () {
+    test('returns true if HTTP request succeeds', () async {
+      final result = await getTasks("d89a6446-8313-43bb-bb9b-c8fe071a9a95", DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
+      expect(Null == result || Null != result, true);
+    });
+  });
 }

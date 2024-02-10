c181f79cdcaf232ee7b14fd971cd1675cf1a8734
Tanmay0215 <134931600+Tanmay0215@users.noreply.github.com>
2024-02-10 22:45:19 +0530

    initial pr (#1)
    Integrated Few Endpoints

--- lib/provider/employee_endpoints.dart
+++ lib/provider/employee_endpoints.dart
@@ -49,3 +49,89 @@ Future<List<dynamic>> getTasks(
         'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
   }
 }
+
+Future<List<dynamic>> getJobDetail(String jobID) async { // under testing
+  final Uri uri = Uri.parse('${getBaseURL()}/employee/$jobID/get-job-detail/');
+  Map<String, String> header = await headers();
+  final response = await http.get(uri, headers: header);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<dynamic> getEmployer() async { // under testing
+  final Uri uri = Uri.parse('${getBaseURL()}/employee/get-employer/');
+  Map<String, String> header = await headers();
+  final response = await http.get(uri, headers: header);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<List<dynamic>> addLocation( // under testing
+    double locationLat, double locationLong) async {
+  final requestData = {
+    "location_lat": locationLat,
+    "location_long": locationLong
+  };
+  final Uri uri = Uri.parse('${getBaseURL()}/employee/add-location/');
+  Map<String, String> header = await headers();
+  final response =
+      await http.post(uri, headers: header, body: jsonEncode(requestData));
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<List<dynamic>> getJobs(double locationLat, double locationLong) async { // under testing
+  final Uri uri = Uri.parse('${getBaseURL()}/employee/get-jobs/');
+  Map<String, String> header = await headers();
+  final requestData = {
+    "location_lat": locationLat,
+    "location_long": locationLong
+  };
+  final response =
+      await http.post(uri, headers: header, body: jsonEncode(requestData));
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else if (response.statusCode == 404) {
+    return [];
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<bool> leaveJob() async { // under testing
+  final Uri uri = Uri.parse('${getBaseURL()}/employee/leave-job/');
+  Map<String, String> header = await headers();
+  final response = await http.post(uri, headers: header);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<bool> approvePayment(String paymentID) async { // under testing
+  final Uri uri =
+      Uri.parse('${getBaseURL()}/employee/$paymentID/approve-payment/');
+  Map<String, String> header = await headers();
+  final response = await http.get(uri, headers: header);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}

1bac69d0a361e113ca41817271b7fa4bb7e8689c
Tanmay0215 <134931600+Tanmay0215@users.noreply.github.com>
2024-02-10 22:45:19 +0530

    initial pr (#1)
    Integrated Few Endpoints

--- lib/provider/user_endpoints.dart
+++ lib/provider/user_endpoints.dart
@@ -31,9 +31,89 @@ Future<bool> createUserOnBackend(
     "user_type": userType,
     "firebase_user_id": firebaseUserId
   };
-  if (email != "") {
-    requestData["email"] = email;
+  Map<String, String> header = await headers();
+  final response =
+      await http.post(uri, headers: header, body: jsonEncode(requestData));
+  print(response.statusCode);
+  if (response.statusCode == 200) {
+    return true;
+  } else if (response.statusCode == 417 || response.statusCode == 409) {
+    return false;
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
   }
+}
+
+Future<dynamic> getUser(// under testing
+    ) async {
+  final Uri uri = Uri.parse('${getBaseURL()}/get-user/');
+  Map<String, String> header = await headers();
+  final response = await http.get(uri, headers: header);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<bool> addRating(String userTo, String rate, String comment) async {// under testing
+  final Uri uri = Uri.parse('${getBaseURL()}/$userTo/add-rating/');
+  final requestData = {"rate": rate, "comment": comment};
+  Map<String, String> header = await headers();
+  final response =
+      await http.post(uri, headers: header, body: jsonEncode(requestData));
+  print(response.statusCode);
+  if (response.statusCode == 200) {
+    return true;
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<bool> getRating(String userTo) async {// under testing
+  final Uri uri = Uri.parse('${getBaseURL()}/$userTo/get-rating/');
+  Map<String, String> header = await headers();
+  final response = await http.get(uri, headers: header);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<bool> getPayments(DateTime startTime, DateTime endTime) async {// under testing
+  startTime = startTime.toUtc();
+  endTime = endTime.toUtc();
+  final requestData = {
+    "start_time": startTime.toIso8601String(),
+    "end_time": endTime.toIso8601String()
+  };
+  final Uri uri = Uri.parse('${getBaseURL()}/user/get-payments/');
+  Map<String, String> header = await headers();
+  final response =
+      await http.post(uri, headers: header, body: jsonEncode(requestData));
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
+
+Future<bool> addFeedback(// under testing
+    {required String fromUserID,
+    required String rating,
+    required String feedback}) async {
+  final Uri uri = Uri.parse('${getBaseURL()}/user/add-feedback/');
+  final requestData = {
+    "from_user_id": fromUserID,
+    "rating": rating,
+    "feedback": feedback
+  };
   Map<String, String> header = await headers();
   final response =
       await http.post(uri, headers: header, body: jsonEncode(requestData));

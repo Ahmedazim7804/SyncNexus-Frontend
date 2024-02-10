d9c3084946960e04da9e33976f26879b1ec9bdbf
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-02-10 22:31:48 +0530

    Formatted Test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:worker_app/provider/base_endpoints.dart';
 import 'package:worker_app/provider/employee_endpoints.dart';
 import 'package:worker_app/provider/employer_endpoints.dart';
 import 'package:worker_app/provider/user_endpoints.dart';
-
 
 void main() {
   group('checkUser', () {
@@ -15,31 +13,37 @@ void main() {
   group('createUser', () {
     test('returns true if HTTP request succeeds', () async {
       final result = await createUserOnBackend(
-          phoneNo: "9315082028", name: "Mahesh", userType: "employee", firebaseUserId: "MM0A6CZw5hVglML2Fk7K0rWaqKn1", email: "ajeem.ahmad.ug23@nsut.ac.in"
-      );
+          phoneNo: "9315082028",
+          name: "Mahesh",
+          userType: "employee",
+          firebaseUserId: "MM0A6CZw5hVglML2Fk7K0rWaqKn1",
+          email: "ajeem.ahmad.ug23@nsut.ac.in");
       expect(result == true || result == false, true);
     });
     test('returns true if HTTP request succeeds', () async {
       final result = await createUserOnBackend(
-          phoneNo: "12345676875", name: "Tony Stark", userType: "employer", firebaseUserId: "vQ0LNIqBZGfTX6Ttoy6INrSiNlc2", email: "apple@gmail.com"
-      );
+          phoneNo: "12345676875",
+          name: "Tony Stark",
+          userType: "employer",
+          firebaseUserId: "vQ0LNIqBZGfTX6Ttoy6INrSiNlc2",
+          email: "apple@gmail.com");
       expect(result == true || result == false, true);
     });
   });
   group('addTask', () {
     test('returns true if HTTP request succeeds', () async {
       final result = await addTask(
-          "1824b2de-bc70-470c-a80f-f52c9da8588e", "Fix the roof", "The roof is leaking", DateTime.now().add(const Duration(days: 3))
-      );
+          "1824b2de-bc70-470c-a80f-f52c9da8588e",
+          "Fix the roof",
+          "The roof is leaking",
+          DateTime.now().add(const Duration(days: 3)));
       expect(result == true || result == false, true);
     });
   });
   group('addEmployee', () {
     test('returns true if HTTP request succeeds', () async {
-      final result = await addEmployee(
-          "1824b2de-bc70-470c-a80f-f52c9da8588e",
-        "Manager"
-      );
+      final result =
+          await addEmployee("1824b2de-bc70-470c-a80f-f52c9da8588e", "Manager");
       expect(result == true || result == false, true);
     });
   });
@@ -51,7 +55,8 @@ void main() {
   });
   group('getLocation', () {
     test('returns true if HTTP request succeeds', () async {
-      final result = await getLocation("1824b2de-bc70-470c-a80f-f52c9da8588e", DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
+      final result = await getLocation("1824b2de-bc70-470c-a80f-f52c9da8588e",
+          DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
       print(result);
       expect(Null == result || Null != result, true);
     });
@@ -64,16 +69,15 @@ void main() {
   });
   group('getTask', () {
     test('returns true if HTTP request succeeds', () async {
-      final result = await getTask(
-          "c2f768c1-d206-4959-912e-0b906f425380"
-      );
+      final result = await getTask("c2f768c1-d206-4959-912e-0b906f425380");
       print(result);
       expect(Null == result || Null != result, true);
     });
   });
   group('getTasks', () {
     test('returns true if HTTP request succeeds', () async {
-      final result = await getTasks("1824b2de-bc70-470c-a80f-f52c9da8588e", DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
+      final result = await getTasks("1824b2de-bc70-470c-a80f-f52c9da8588e",
+          DateTime.now().subtract(const Duration(days: 3)), DateTime.now());
       print(result);
       expect(Null == result || Null != result, true);
     });
@@ -85,33 +89,26 @@ void main() {
       expect(Null != result, true);
     });
   });
-<<<<<<< HEAD
-}
-||||||| e76931d
-}
-=======
   group('addJobs', () {
     test('returns true if HTTP request succeeds', () async {
-      final result = await addJobs("description, I'm iron man @#^54192379", "title", 28.704060, 77.102493, 20000);
+      final result = await addJobs("description, I'm iron man @#^54192379",
+          "title", 28.704060, 77.102493, 20000);
       expect(result, true);
     });
   });
   group('addPayments', () {
     test('returns true if HTTP request succeeds', () async {
-      final result = await addPayments(
-        "1824b2de-bc70-470c-a80f-f52c9da8588e", "Tea/Coffee for FY23", "USD", 100000
-      );
+      final result = await addPayments("1824b2de-bc70-470c-a80f-f52c9da8588e",
+          "Tea/Coffee for FY23", "USD", 100000);
       expect(true == result, true);
     });
   });
   group('getEmployeePayments', () {
     test('returns true if HTTP request succeeds', () async {
-      final result = await getEmployeePayments("1824b2de-bc70-470c-a80f-f52c9da8588e");
+      final result =
+          await getEmployeePayments("1824b2de-bc70-470c-a80f-f52c9da8588e");
       print(result);
       expect(Null != result, true);
     });
   });
-
-
 }
->>>>>>> 86ceac9

7c77e6c2b3c44c3f2cbb8a9b5d814a29403f0485
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-02-12 02:46:02 +0530

    Added Find User By ID

--- lib/provider/user_endpoints.dart
+++ lib/provider/user_endpoints.dart
@@ -122,3 +122,15 @@ Future<bool> addFeedback(int rating, String feedback) async {
         'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
   }
 }
+
+Future<dynamic> getUserByID(String userID) async {
+  final Uri uri = Uri.parse('${getBaseURL()}/user/$userID/fetch-user-by-id/');
+  Map<String, String> header = await headers();
+  final response = await http.get(uri, headers: header);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception(
+        'Failed to load data from endpoint: ${response.statusCode} ${response.body}');
+  }
+}
